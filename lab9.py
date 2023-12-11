c0e3659bd4bde011ba4d418e5dc0486a46901ea4
Cian Martyn <G00404885@atu.ie>
2023-12-11 17:40:40 +0000

    Shi Tomasi

--- lab9.py
+++ lab9.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 
 imgIn = cv2.imread('ATU1.jpg')
 
+imgGrey = cv2.cvtColor(imgIn, cv2.COLOR_BGR2GRAY)
+
 # Create a deep copy of the original image
 imgHarris = imgIn.copy()
-
-imgGrey = cv2.cvtColor(imgIn, cv2.COLOR_BGR2GRAY)
 
 # Create a figure to display the images
 plt.figure(figsize=(12, 8))
@@ -46,10 +46,33 @@ for i in range(len(dst)):
             # Set appropriate B, G, R values for the circle color (e.g., Red)
             cv2.circle(imgHarris, (j, i), 3, (0, 0, 255), -1)
 
-# Create subplot for the corner-marked image
+# Plot Harris corners
 plt.subplot(2, 3, 3)
 plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB))
 plt.title('Harris Corner Detection')
 plt.xticks([]), plt.yticks([])
+
+# Perform Shi-Tomasi corner detection 
+maxCorners = 100 
+qualityLevel = 0.01
+minDistance = 10
+
+corners = cv2.goodFeaturesToTrack(imgGrey, maxCorners, qualityLevel, minDistance)
+
+# Convert corners to integer coordinates
+corners = np.int0(corners)
+
+# Loop through the corners and draw circles on the image
+for corner in corners:
+    x, y = corner.ravel()
+    cv2.circle(imgGrey, (x, y), 3, 255, -1) 
+
+# Plot Shi-Tomasi  image
+plt.subplot(2, 3, 4)
+plt.imshow(imgGrey, cmap='gray')
+plt.title('Shi-Tomasi Corner Detection')
+plt.xticks([]), plt.yticks([])
+
+plt.show()
 
 plt.show()
